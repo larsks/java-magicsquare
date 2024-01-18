ee9fe82c9bab849c65bc9ee63aac9a283caee02c
Lars Kellogg-Stedman <lars@oddbit.com>
2024-01-18 07:35:13 -0500

    Better test names for parameterized tests

--- app/src/test/java/magicsquare/MagicSquareTest.java
+++ app/src/test/java/magicsquare/MagicSquareTest.java
@@ -2,11 +2,13 @@ package magicsquare;
 
 import matrix.Matrix;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MagicSquareTest {
+	@DisplayName("A 3x3 magic square should have expected values")
 	@Test
 	public void testThreeByThree() {
 		int want_values[][] = {{8,1,6},{3,5,7},{4,9,2}};
@@ -21,12 +23,13 @@ public class MagicSquareTest {
 		}
 	}
 
-	@ParameterizedTest
+	@ParameterizedTest(name = "[{index}] A size of {0} should trigger an InvalidSizeException")
 	@ValueSource(ints = {1, 8, 20}) // six numbers
 	public void testInvalidSize(int size) {
 		assertThrows(InvalidSizeException.class, () -> new MagicSquare(size));
 	}
 
+	@DisplayName("A valid size should not cause any errors")
 	@Test
 	public void testCreateValidSize() {
 		assertDoesNotThrow(() -> new MagicSquare(3));
